30938b8d23a7a2eff6807050bdf89e7cfcd0cd8d
batuhan <batuhan.gocmen@hotmail.com>
2021-09-27 14:17:24 +0300

    Initial Unity Project Commit
    Storing all of our Project that build our with unity

---
+++ Assets/Scipts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]float RotationAmount=100f;
+    [SerializeField]float ThrustAmount=100f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+  
+    void ProcessRotation()
+    {
+         if (Input.GetKey(KeyCode.A))
+        {
+            RotationMethod(RotationAmount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotationMethod(-RotationAmount);
+        }
+    }
+
+  void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up*ThrustAmount*Time.deltaTime);
+        }
+
+    }
+     void RotationMethod(float rotationThisFrame)
+    {
+        rb.freezeRotation=true;//Freezing rotation so we can manually rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation=false;//unFreezing rotation so physics system can take over
+    }
+}
